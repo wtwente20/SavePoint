f53db7adef7c354a2a6628604b0b0b3f44ade8f2
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-08-23 14:08:49 -0500

    swapped placeholder logo, working on making it appear at launch

---
+++ lib/screens/forgot_password_screen.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  @override
+  _ForgotPasswordScreenState createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+
+  Future<void> _resetPassword() async {
+    try {
+      await _auth.sendPasswordResetEmail(email: _email);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Reset password link has been sent to your email.")));
+      Navigator.of(context).pop();
+    } catch (error) {
+      print(error);
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Error sending reset password link.")));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Reset Password')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Text("Enter your email to receive a password reset link."),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                onSaved: (value) => _email = value!,
+                validator: (value) {
+                  if (value!.isEmpty) return "Please enter your email.";
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                child: Text('Send Reset Link'),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    _resetPassword();
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,7 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart'; // Ensure you have imported this package.
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:savepoint_app/screens/sign_up_screen.dart'; // Ensure you have imported this package.
+import 'package:savepoint_app/screens/sign_up_screen.dart';
+
+import 'forgot_password_screen.dart';
 
 class SignInScreen extends StatefulWidget {
   @override
@@ -11,64 +13,65 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn googleSignIn = GoogleSignIn();
-  
+
   final _formKey = GlobalKey<FormState>();
   String _email = '';
   String _password = '';
 
-  Future<User?> _signInWithGoogle() async {
-    try {
-      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+  // Future<User?> _signInWithGoogle() async {
+  //   try {
+  //     final GoogleSignInAccount? googleSignInAccount =
+  //         await googleSignIn.signIn();
 
-      if (googleSignInAccount != null) {
-        final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+  //     if (googleSignInAccount != null) {
+  //       final GoogleSignInAuthentication googleSignInAuthentication =
+  //           await googleSignInAccount.authentication;
 
-        final AuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleSignInAuthentication.accessToken,
-          idToken: googleSignInAuthentication.idToken,
-        );
+  //       final AuthCredential credential = GoogleAuthProvider.credential(
+  //         accessToken: googleSignInAuthentication.accessToken,
+  //         idToken: googleSignInAuthentication.idToken,
+  //       );
 
-        final userCredential = await _auth.signInWithCredential(credential);
-
-        return userCredential.user;
-      }
-    } catch (error) {
-      print(error);
-      return null;
-    }
-  }
+  //       final userCredential = await _auth.signInWithCredential(credential);
+  //       return userCredential.user;
+  //     }
+  //   } catch (error) {
+  //     print(error);
+  //     return null;
+  //   }
+  // } disabled for now
 
   Future<void> _signInWithEmailAndPassword() async {
-  if (_formKey.currentState!.validate()) {
-    _formKey.currentState!.save();
-    try {
-      await _auth.signInWithEmailAndPassword(email: _email, password: _password);
-      Navigator.of(context).pushReplacementNamed('/home');
-    } on FirebaseAuthException catch (e) { // Catch FirebaseAuthException
-      print('Error code: ${e.code}');
-      String errorMessage;
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      try {
+        await _auth.signInWithEmailAndPassword(
+            email: _email, password: _password);
+        Navigator.of(context).pushReplacementNamed('/home');
+      } on FirebaseAuthException catch (e) {
+        print('Error code: ${e.code}');
+        String errorMessage;
+        switch (e.code) {
+          case 'user-not-found':
+            errorMessage = 'No user found with this email.';
+            break;
+          case 'wrong-password':
+            errorMessage = 'Incorrect password.';
+            break;
+          default:
+            errorMessage = e.message!;
+            break;
+        }
 
-      // Use switch-case to match against the error codes and provide a more friendly message.
-      switch (e.code) {
-        case 'user-not-found':
-          errorMessage = 'No user found with this email.';
-          break;
-        case 'wrong-password':
-          errorMessage = 'Incorrect password.';
-          break;
-        default:
-          errorMessage = e.message!;
-          break;
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(errorMessage)));
+      } catch (error) {
+        print(error);
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Error logging in. Please try again.")));
       }
-
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(errorMessage)));
-    } catch (error) {
-      print(error);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error logging in. Please try again.")));
     }
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
@@ -78,52 +81,58 @@ class _SignInScreenState extends State<SignInScreen> {
         padding: const EdgeInsets.all(20.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            children: [
-              // Placeholder for your logo
-              Container(
-                height: 200,
-                width: 200,
-                color: Colors.grey[300],
-                child: Center(child: Text('Logo Placeholder')),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                child: Text('Sign in with Google'),
-                onPressed: _signInWithGoogle,
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Email'),
-                onSaved: (value) => _email = value!,
-                validator: (value) {
-                  if (value!.isEmpty) return "Please enter your email.";
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                onSaved: (value) => _password = value!,
-                validator: (value) {
-                  if (value!.isEmpty) return "Please enter your password.";
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                child: Text('Login'),
-                onPressed: _signInWithEmailAndPassword,
-              ),
-              SizedBox(height: 10),
-              TextButton(
-                child: Text('Don\'t have an account? Register here!'),
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => SignUpScreen()));
-                },
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset(
+                      'assets/SavepointLogo1.png'),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                  onSaved: (value) => _email = value!,
+                  validator: (value) {
+                    if (value!.isEmpty) return "Please enter your email.";
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  onSaved: (value) => _password = value!,
+                  validator: (value) {
+                    if (value!.isEmpty) return "Please enter your password.";
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextButton(
+                  child: Text('Forgot password?'),
+                  onPressed: () {
+                    // Navigate to ForgotPasswordScreen
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (ctx) => ForgotPasswordScreen()));
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  child: Text('Login'),
+                  onPressed: _signInWithEmailAndPassword,
+                ),
+                SizedBox(height: 10),
+                TextButton(
+                  child: Text('Don\'t have an account? Register here!'),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (ctx) => SignUpScreen()));
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
